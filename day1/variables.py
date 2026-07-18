42244cb50102904d873fd8a7f856280227591a95
Dnyaneshwar Chandwade <dnyaneshwaar.chandwade@gmail.com>
2026-07-18 19:15:14 +0530

    added variable test file

---
+++ day1/variables.py
@@ -0,0 +1,25 @@
+# Data type - int,float,str,list,tuple,dict,set,bool
+#
+# a=10
+# b=20.5
+# grade ="A"
+# S = "welcome"
+#
+# print(type(a))
+# print(type(b))
+# print(type(grade))
+# print(type(S))
+# print(a,b,grade)
+#
+# p,q,r=1,2,"multi"
+# print(p,q,r)
+
+#Swaping the numbers
+a=10
+b=20
+
+print(a,b) #before swap
+
+a,b=b,a #swaping logic in python
+print(a,b) #After swap
+
